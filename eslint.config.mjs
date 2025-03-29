a1478737adb9d198d6f0d29a81e420932dd70c9a
Dev. Sammie Barasa <sammie.barasa@students.jkuat.ac.ke>
2025-03-29 23:46:30 +0300

    update eslint configuration to support latest ECMAScript features and module syntax

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,18 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const compat = new FlatCompat();
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+  },
+];
 
 export default eslintConfig;
